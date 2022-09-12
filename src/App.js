e149b2921ea206c1e753e4573e7317f04456885d
Binary44 <binarycoder.com@gmail.com>
2022-09-12 16:04:19 +0100

    inprovinig

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { useState } from "react";
 
-
 function App() {
   const [result, setResult] = useState("");
 
   return (
     <div className="App">
-     <div className="container">
-      <form>
-        <input type="text" value={result} />
-      </form>
-     </div>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+
+        <div className="keypad">
+          <button>clear</button>
+          <button>c</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash;</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
